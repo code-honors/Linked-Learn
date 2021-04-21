e4b2c76044b5b816fef532dc2d1ffb16705f62d6
BasharNofal <bashar.nofal1997@gmail.com>
2021-04-22 01:44:45 +0300

    added google oauth

--- src/server.js
+++ src/server.js
@@ -4,16 +4,50 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 const notFoundHandler = require('./error-handlers/404.js');
 const errorHandler = require('./error-handlers/500.js');
 const authRoutes = require('./auth/routes.js');
+const passport = require('passport');
 
 const app = express();
+const SECRET = process.env.SECRET;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: SECRET,
+  callbackURL: "/"
+},
+  function (accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/', (req, res) => {
+  res.send('home');
+});
+
+// app.get()
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 app.use(authRoutes);
 app.use('*', notFoundHandler);
