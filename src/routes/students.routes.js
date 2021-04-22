d3bc28180f2d3da65e8a577e76db959c14740bd1
rubabanat <rubabanat1996@gmail.com>
2021-04-23 00:06:04 +0300

    edit

--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -5,13 +5,13 @@ const db = require("../db");
 
 router.get("/", async function(req, res, next) {
     try {
-      const results = await db.query("SELECT * FROM courses ");
+      const results = await db.query("SELECT * FROM students_courses; ");
       return res.json(results.rows);
     } catch (err) {
       return next(err);
     }
   });
-  
+
   router.delete("/:id", async function(req, res, next) {
     try {
       const result = await db.query("DELETE FROM  WHERE id=$1", [
